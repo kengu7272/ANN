4703e40fc18b0d6f7e96344c1a91e245edd12980
kengu7272 <kengu72@gmail.com>
2023-10-07 17:35:25 -0400

    worked out some issues with setup and added nodemon for live reloads with development

--- ann/next.config.js
+++ ann/next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    serverRuntimeConfig: {
+      // Specify the path to your custom entry file
+      serverEntry: './server/server.mjs', // Replace with your actual file path
+    },
+    publicRuntimeConfig: {
+      // Add any public runtime configuration here
+    },
+  };
+  
--- ann/tailwind.config.ts
+++ ann/tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
   },
   plugins: [],
 }
