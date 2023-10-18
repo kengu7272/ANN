e185dff4cb6569cf4e1be48ab9f2f2b75d0bb711
kengu7272 <kengu72@gmail.com>
2023-10-17 22:10:53 -0400

    edited header centering

--- ann/src/app/home/page.tsx
+++ ann/src/app/home/page.tsx
@@ -5,17 +5,15 @@ export default function Home() {
     return (
     <div className="bgImage bg-center bg-cover h-full w-full">
         <header className="bg-transparent text-teal-300 sticky top-0 z-10">
-            <section className="max-w-10xl mx-auto p-4 flex justify-between items-center">
-                <h1 className="text-3xl font-medium">
+            <section className="max-w-10xl mx-auto p-4 flex flex-row tablet:justify-center items-center">
+                <h1 className="text-3xl font-medium absolute left-0">
                     <a href="#hero">ANN</a>
                 </h1>
-                <div>
-                    <nav className="sm:block space-x-8 text-xl text-white" aria-label="main">
-                        <a href="#Create" className="hover:opacity-50">Create</a>
-                        <a href="#Edit" className="hover:opacity-50">Edit</a>
-                        <a href="#Manage" className="hover:opacity-50">Manage</a>
-                        </nav>
-                </div>
+                <nav className="sm:block space-x-8 text-xl ml-auto tablet:ml-0 text-white" aria-label="main">
+                    <a href="#Create" className="hover:opacity-50">Create</a>
+                    <a href="#Edit" className="hover:opacity-50">Edit</a>
+                    <a href="#Manage" className="hover:opacity-50">Manage</a>
+                </nav>
             </section>
         </header>
         <header className="bg-transparent text-teal-300 sticky top-0 z-10">
