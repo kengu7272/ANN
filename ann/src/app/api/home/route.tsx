2be6cdfff58e9cf3ea254bd4b76f4625fe169cdb
dnamenyi <dnamenyi@outlook.com>
2023-11-27 12:59:43 -0500

    misc changes

--- ann/src/app/api/home/route.tsx
+++ ann/src/app/api/home/route.tsx
@@ -20,9 +20,31 @@ export async function GET(req: Request) {
         });
     }
 
-    const { userid } = jwt.verify(token, process.env.SECRET_KEY!) as JwtPayload;
+    try {
+        const { userid } = jwt.verify(token, process.env.SECRET_KEY!) as JwtPayload;
 
-    return Response.json({
-        status: 207
-    })
+        // Replace this query with your logic to fetch the trivia question from the database
+        const [songResult]: [RowDataPacket[], FieldPacket[]] = await db.query(
+            'SELECT songs.songid, songs.title as title, artists.name as artist FROM songs JOIN artists ON songs.artistid = artists.artistid ORDER BY RAND() LIMIT 1'
+        );
+
+        if (!songResult || songResult.length === 0) {
+            return Response.json({
+                status: 404,
+                error: 'Trivia question not found'
+            });
+        }
+
+        const triviaQuestion = songResult[0].question;
+
+        return Response.json({
+            status: 200,
+            question: triviaQuestion
+        });
+    } catch (error) {
+        return Response.json({
+            status: 500,
+            error: 'Internal Server Error'
+        });
+    }
 }
--- ann/src/app/home/page.tsx
+++ ann/src/app/home/page.tsx
@@ -12,17 +12,29 @@ interface Question {
 
 export default function Home() {
     const [token, setToken] = useState('');
+    const [question, setQuestion] = useState<string | null>(null);
     const router = useRouter();
 
-    const getQuestion = async() => {
-        const response = await fetch('/api/home', {
-            method: 'GET',
-            headers: {
-                'Authorization': token
-            }
-        });
+    const getQuestion = async () => {
+        try {
+            const response = await fetch('/api/home', {
+                method: 'GET',
+                headers: {
+                    'Authorization': token
+                }
+            });
 
-        //const data = await response.json();
+            const data = await response.json();
+
+            if (response.ok) {
+                setQuestion(data.question);
+            } else {
+                // Handle error
+                console.error(data.error);
+            }
+        } catch (error) {
+            console.error("Error fetching question:", error);
+        }
     }
 
     useEffect(() => {
@@ -44,10 +56,11 @@ export default function Home() {
             <main className="bg-neutral-900 border-2 flex h-3/4 justify-center items-center desktop:max-h-[600px] max-w-[900px] opacity-90 shadow-neutral-900 shadow-2xl w-[90%] laptop:w-1/2">
                 <form className="bg-transparent flex flex-col gap-8 h-[70%] tablet:h-4/5 items-center justify-center tablet:text-lg w-[95%]">
                     <label className="text-3xl tablet:text-4xl">Music Trivia</label>
-                    <input required type="text" placeholder="Answer" className="h-[10%] p-2 rounded-xl text-neutral-900 w-4/5"/>
+                    <p className="">{question ? `What artist wrote ${question}?` : 'Loading question...'}</p>
+                    <input required type="text" placeholder="Answer" className="h-[10%] p-2 rounded-xl text-neutral-900 w-4/5" />
                     <input type="submit" value="Submit" className="bg-neutral-700 hover:bg-neutral-600 active:bg-neutral-800 h-1/6 mx-2 rounded-xl w-1/2" />
                 </form>
             </main>
-        </div> 
+        </div>
     )
 }
